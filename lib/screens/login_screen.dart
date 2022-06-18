4cca6cc11282855b323d9664b9c0dd24ec0405bf
jose09511 <josemanuelbarbasanchez@gmail.com>
2022-06-18 17:40:03 +0200

    login with google

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,7 +4,9 @@ import 'package:authentication_flutterfire/components/apple_sign_in_button.dart'
 import 'package:authentication_flutterfire/components/email_sign_in_button.dart';
 import 'package:authentication_flutterfire/components/facebook_sign_in_button.dart';
 import 'package:authentication_flutterfire/components/google_sign_in_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -47,7 +49,29 @@ class LoginScreen extends StatelessWidget {
                     margin: const EdgeInsets.all(8),
                     child: GoogleSignInButton(
                       darkMode: darkMode,
-                      onPressed: () {},
+                      onPressed: () {
+                        Future<UserCredential> signInWithGoogle() async {
+                          // Trigger the authentication flow
+                          final GoogleSignInAccount? googleUser =
+                              await GoogleSignIn().signIn();
+
+                          // Obtain the auth details from the request
+                          final GoogleSignInAuthentication? googleAuth =
+                              await googleUser?.authentication;
+
+                          // Create a new credential
+                          final credential = GoogleAuthProvider.credential(
+                            accessToken: googleAuth?.accessToken,
+                            idToken: googleAuth?.idToken,
+                          );
+
+                          // Once signed in, return the UserCredential
+                          return await FirebaseAuth.instance
+                              .signInWithCredential(credential);
+                        }
+
+                        signInWithGoogle();
+                      },
                     ),
                   ),
                   Container(
